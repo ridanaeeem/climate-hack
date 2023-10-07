424bc13827bbf1419e4a360a9c18c1bce4556fdf
KingTingTheGreat <jting@bu.edu>
2023-10-07 16:45:48 -0400

    idk

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,35 +1,62 @@
-const USERS = new Map();
-
-USERS.set("jting@bu.edu", "password");
-
-
-
-function testAlert() {
-    alert("This is a TEST. DO NOT PANIC!!!");
-}
+// function getUSERS() {
+//     let USERS = localStorage.getItem("USERS");
+//     if (USERS === null) {
+//         USERS = new Map();
+//         localStorage.setItem("USERS", USERS);
+//     }
+//     return USERS;
+// }
 
 function getEmailPwd() {
     let email = document.getElementsByName("email")[0].value;
-    let password = document.getElementsByName("psw")[0].value;
+    let password = document.getElementsByName("password")[0].value;
     return [email, password];
+}
+
+function getName() {
+    return document.getElementsByName("name")[0].value;
+}
+
+function createNewUser(email, password) {
+    let newUser = {};
+    newUser.email = email;
+    newUser.password = password;
+    // newUser.name = getName();
+    newUser.points = 0;
+    return newUser;
 }
 
 function signUp() {
     let [email, password] = getEmailPwd();
-    let msg = `Email: ${email}\nPassword: ${password}`;
+    let USERS = getUSERS();
     if (USERS.has(email)) {
         alert("This email is already associated with an account.");
         return;
     }
-    alert(msg);
-//     alert("Sign Up Successful!");
+    USERS.set(email, createNewUser(email, password));
+    localStorage.setItem("USERS", USERS);
+    localStorage.setItem("currentUser", email);
+    localStorage.setItem("currentPassword", password);
+    localStorage.setItem("currentName", getName());
+    localStorage.setItem("currentPoints", 0);
+    alert("Account created successfully.")
+    return;
 }
 
 function login() {
-    let email, password = getEmailPwd();
+    let [email, password] = getEmailPwd();
+    let USERS = getUSERS();
     if (!USERS.has(email)) {
         alert("This email is not associated with an account.");
         return;
     }
-    console.log(email);
+    if (!(USERS.get(email).password === password)) {
+        alert("Incorrect password.");
+        return;
+    }
+    localStorage.setItem("currentUser", email);
+    localStorage.setItem("currentPassword", password);
+    localStorage.setItem("currentName", USERS.get(email).name);
+    localStorage.setItem("currentPoints", USERS.get(email).points);
+    return;
 }
